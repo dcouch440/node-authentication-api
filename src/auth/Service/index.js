f6acf6f9f00d71e96576153c580aa61cee4d1453
dcouch440 <dcouch440@gmail.com>
2021-06-19 19:07:59 -0700

    add service

---
+++ src/auth/Service/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { default: axios } = require('axios');
+const jwt = require('jsonwebtoken');
+const credentials = require('../../../credentials');
+const verifyOptions = require('../config/verifyOptions');
+const { forwardServerUrl } = require('../config/options')
+
+class Service {
+  // validate
+  verify ({ token }) {
+    try {
+      jwt.verify(token, credentials.publicKey, verifyOptions);
+      return jwt.decode(token, { complete: true })
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+  // re sign before request
+  sign ({ payload }) {
+    try {
+      return jwt.sign(payload, credentials.privateKey, signOptions);
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+  // send via request?
+  sendRequest ({ token }) {
+    return axios
+      .post(
+        forwardServerUrl,
+        { withCredentials: true },
+        { headers: { token } }
+      );
+  }
+}
+
+module.exports = new Service();
